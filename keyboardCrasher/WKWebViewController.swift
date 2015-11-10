48ae2bd7a6e1cc26b1add26d5ebe96143f3639fe
Toshiyuki Masuda <ochimasu@gmail.com>
2015-11-10 17:47:42 +0900

    wkWebView表示用の ViewControllerの追加

---
+++ keyboardCrasher/WKWebViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WKWebViewController.swift
+//  keyboardCrasher
+//
+//  Created by masuda on 2015/11/10.
+//  Copyright © 2015年 ignition. All rights reserved.
+//
+
+import UIKit
+
+class WKWebViewController: UIViewController {
+
+    @IBOutlet private weak var webViewBaseView: UIView!
+}
