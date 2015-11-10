ccdbf4542841ea63c9efbfa5cbf8cf00a51a57fc
Toshiyuki Masuda <ochimasu@gmail.com>
2015-11-10 18:19:49 +0900

    WKWebViewを追加して Googleを表示させるように

--- keyboardCrasher/WKWebViewController.swift
+++ keyboardCrasher/WKWebViewController.swift
@@ -7,8 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class WKWebViewController: UIViewController {
 
     @IBOutlet private weak var webViewBaseView: UIView!
+    private weak var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        let webView = WKWebView()
+        webView.frame = webViewBaseView.bounds
+        webView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        webViewBaseView.addSubview(webView)
+        self.webView = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString = "https://www.google.co.jp"
+        let request = NSURLRequest(URL: NSURL(string: urlString)!)
+        webView.loadRequest(request)
+    }
 }
